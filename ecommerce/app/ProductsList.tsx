040668a7ce3a3e0e08b2f9612bbf637451bb3567
Simon van Tulder <vantulder.simon@gmail.com>
2025-08-06 16:16:46 -0700

    fix Product list page

--- ecommerce/app/ProductsList.tsx
+++ ecommerce/app/ProductsList.tsx
@@ -7,7 +7,7 @@ export default function ProductsList({products}:{ products: Product[]}){
         <div>
             {products.map(product => (
                 <div key={product.id}>
-                    <Link key={product.id} href="/product-detail"/>
+                    <Link key={product.id} href={"/products" + product.id}/>
                     <Image src={`/` + product.imageUrl} alt="Product image" width={150} height={150}/>
                     <h2>{product.name}</h2>
                     <p>${product.price}</p>
--- ecommerce/app/products/[id]/page.tsx
+++ ecommerce/app/products/[id]/page.tsx
@@ -1,3 +1,6 @@
-export default function ProductDetailPage(){
-    return <h1> ProductDetail </h1>
+import { products } from "@/app/product-data"
+
+export default function ProductDetailPage({ params } : { params : {id : string}}){
+    const product = products?.find(p => p.id ===params.id);
+    return <h1> {product?.name} </h1>
 }
