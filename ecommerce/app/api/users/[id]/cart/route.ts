8ffd36d50ff71664f8f2732841de9c55c2b074e6
Simon van Tulder <vantulder.simon@gmail.com>
2025-08-09 15:04:24 -0700

    build add to shopping cart endpoint

--- ecommerce/app/api/users/[id]/cart/route.ts
+++ ecommerce/app/api/users/[id]/cart/route.ts
@@ -5,7 +5,8 @@ type ShoppingCart = Record<string, string[]>;
 
 const carts: ShoppingCart = {
     '1': ['123', '234'],
-    '2': ['234', '456']
+    '2': ['234', '456'],
+    '3': ['234']
 }
 type Params = {
     id: string;
@@ -14,7 +15,7 @@ type Params = {
 export async function GET(request:NextRequest, { params }: {params: Params}){
     const userID = params.id;
     const productIDs = carts[userID];
-    
+
     if(productIDs === undefined){
         return new Response(JSON.stringify([]), {
             status:200,
@@ -28,6 +29,25 @@ export async function GET(request:NextRequest, { params }: {params: Params}){
         status: 200,
         headers: {
             'Content-Type': 'applications/json'
+        }
+    })
+}
+type CartBody = { 
+    productID: string 
+};
+
+export async function POST(request: NextRequest, { params }: {params : Params} ){
+    const userID = params.id;
+    const body: CartBody = await request.json();
+    const productID = body.productID;
+
+    carts[userID] = carts[userID] ? carts[userID].concat(productID) : [productID];
+    const cartProducts = carts[userID].map(id => products.find(p => p.id ===id))
+
+    return new Response(JSON.stringify(cartProducts), {
+        status:201,
+        headers: {
+            'Content-Type' : 'application/json'
         }
     })
 }
